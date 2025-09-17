c26d827fd7c95c215303071ea9446145b2c02f6c
ShwaTech <shwakesh13579@gmail.com>
2025-09-17 14:37:11 +0300

    Experiment Tracking With MLFlow - Register a Custom Model

--- src/marvel_characters/models/custom_model.py
+++ src/marvel_characters/models/custom_model.py
@@ -37,7 +37,8 @@ class MarvelModelWrapper(mlflow.pyfunc.PythonModel):
         code_paths: list[str],
         input_example: pd.DataFrame,
     ) -> None:
-        """Log and register the model.
+        """
+        Log and register the model.
 
         :param wrapped_model_uri: URI of the wrapped model
         :param pyfunc_model_name: Name of the PyFunc model
@@ -46,15 +47,20 @@ class MarvelModelWrapper(mlflow.pyfunc.PythonModel):
         :param code_paths: List of code paths
         :param input_example: Input example for the model
         """
+        
         mlflow.set_experiment(experiment_name=experiment_name)
+        
         with mlflow.start_run(run_name=f"wrapper-lightgbm-{datetime.now().strftime('%Y-%m-%d')}", tags=tags.to_dict()):
             additional_pip_deps = []
+            
             for package in code_paths:
                 whl_name = package.split("/")[-1]
                 additional_pip_deps.append(f"code/{whl_name}")
+            
             conda_env = _mlflow_conda_env(additional_pip_deps=additional_pip_deps)
-
+            
             signature = infer_signature(model_input=input_example, model_output={"Survival prediction": ["alive"]})
+            
             model_info = mlflow.pyfunc.log_model(
                 python_model=self,
                 name="pyfunc-wrapper",
@@ -63,16 +69,21 @@ class MarvelModelWrapper(mlflow.pyfunc.PythonModel):
                 code_paths=code_paths,
                 conda_env=conda_env,
             )
+        
         client = MlflowClient()
+        
         registered_model = mlflow.register_model(
             model_uri=model_info.model_uri,
             name=pyfunc_model_name,
             tags=tags.to_dict(),
         )
+        
         latest_version = registered_model.version
+        
         client.set_registered_model_alias(
             name=pyfunc_model_name,
             alias="latest-model",
             version=latest_version,
         )
+        
         return latest_version
